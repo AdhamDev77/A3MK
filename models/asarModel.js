aae7c72fbc33fc87c3672e8caa20b383b34d7e7a
AdhamDev77 <developedbyadham7@gmail.com>
2024-04-02 17:49:27 +0300

    first commit

---
+++ models/asarModel.js
@@ -0,0 +1,48 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema
+
+const asarSchema = new Schema({
+    project_info: {
+        type: Object,
+        required: true
+    },
+    project_goals_1: {
+        type: Object,
+        required: true
+    },
+    project_goals_2: {
+        type: Object,
+        required: true
+    },
+    project_tahlils: {
+        type: Object,
+        required: true
+    },
+    m3neen: {
+        type: Object,
+        required: true
+    },
+    project_natiga: {
+        type: Object,
+        required: true
+    },
+    mod5alat: {
+        type: Number,
+        required: true
+    },
+    kema_mogtama3ya: {
+        type: Number,
+        required: true
+    },
+    safy_kema_mogtama3ya: {
+        type: Number,
+        required: true
+    },
+    aed: {
+        type: Number,
+        required: true
+    },
+}, {timestamps: true})
+
+module.exports = mongoose.model('Asar', asarSchema)
---
+++ routes/users.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const Users = require('../models/userModel')
+const requireAuth = require('../middleware/requireAuth')
+const {createUser,logUser,getUsers,getOneUser,deleteUser,updateUser, getMosReq, getMos,changePassword} = require("../Controllers/usersController")
+
+const router = express.Router()
+
+//router.use(requireAuth)
+
+router.get('/', getUsers)
+
+router.get('/getMos', getMos)
+
+router.get('/getMosReq', getMosReq)
+
+router.post('/signup', createUser)
+
+router.post('/login', logUser)
+
+router.post('/change_password/:id', changePassword)
+
+router.get('/:id', getOneUser)
+
+router.delete('/:id', deleteUser)
+
+router.patch('/:id', updateUser)
+
+module.exports = router
